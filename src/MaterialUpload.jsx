d4c21b79ec14481bf2441136afa2a3329fd46dcc
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2024-06-26 10:18:45 +0530

    can now extract API from ebnv file

--- src/MaterialUpload.jsx
+++ src/MaterialUpload.jsx
@@ -1,17 +1,13 @@
 
 import lighthouse from '@lighthouse-web3/sdk'
 
+
 function MaterialUpload() {
 
-  const progressCallback = (progressData) => {
-    let percentageDone =
-      100 - (progressData?.total / progressData?.uploaded)?.toFixed(2)
-    console.log(percentageDone)
-  }
 
   const uploadFile = async(file) =>{
 
-    const output = await lighthouse.upload(file, "API_KEY", false, null, progressCallback)
+    const output = await lighthouse.upload(file, import.meta.env.VITE_LIGHTHOUSE_API_DOCUMENT_STORAGE, false, null)
     console.log('File Status:', output)
 
 
