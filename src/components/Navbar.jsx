cf47710a171c503d4a3a2fd9629a77702bbe0a11
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2024-06-28 10:25:47 +0530

    navbar styles updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 // import PropTypes from "prop-types"
+import "../styles/Navbar.css"
 import ConnectWallet from "./ConnectWallet"
 import Button from "./Button"
 const Navbar = () => {
   return (
     <div className="NavbarMain">
         <div className="logo"> <Button ButtonName="logo" /> </div>
-        <div className="aboutUs"><Button ButtonName="About us" /></div>
-        <div className="PdfToVideo"><Button ButtonName="Pdf to Video" /></div>
-        <div className="connectWalletButton"><ConnectWallet /></div>
+        <div className="Buttons">
+          <div className="aboutUs"><Button ButtonName="About us" /></div>
+          <div className="PdfToVideo"><Button ButtonName="Pdf to Video" /></div>
+          <div className="connectWalletButton"><ConnectWallet /></div>
+        </div>
     </div>
   )
 }
